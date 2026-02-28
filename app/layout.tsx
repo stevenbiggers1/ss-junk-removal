e5ee8c5ed0721cc7683fa81e61e939bcb197396a
Steven Biggers <steven.biggers1@gmail.com>
2026-02-28 13:20:46 -0600

    Add Google Analytics tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,20 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+
+        <Script
+    src="https://www.googletagmanager.com/gtag/js?id=G-KXZ46DJ5NZ"
+    strategy="afterInteractive"
+  />
+
+  <Script id="google-analytics" strategy="afterInteractive">
+    {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-KXZ46DJ5NZ');
+    `}
+  </Script>
         {children}
       </body>
     </html>
